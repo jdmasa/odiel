d49de61f42025e3c41e240d4a3793e2773141c4d
jdmasa <47989615+jdmasa@users.noreply.github.com>
2026-08-19 12:33:18 +0200

    Update Vite config to set base path
    Removed server configuration and added base path.

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-    hmr: {
-      port: 3000,
-    },
+  base: '/odiel/',
   },
 });
